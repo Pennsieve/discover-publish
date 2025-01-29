4697165baa7ae996595896378b1ac6194ad4fac6
ddefreitas.exe <1959720+darrell-d@users.noreply.github.com>
2025-01-29 12:29:31 -0500

    update policies to add afs1 discover and embargo buckets

--- discover-publish/terraform/iam.tf
+++ discover-publish/terraform/iam.tf
@@ -127,8 +127,12 @@ data "aws_iam_policy_document" "ecs_task_iam_policy_document" {
       "${data.terraform_remote_state.platform_infrastructure.outputs.precision_publish50_bucket_arn}/*",
       data.terraform_remote_state.platform_infrastructure.outputs.precision_embargo50_bucket_arn,
       "${data.terraform_remote_state.platform_infrastructure.outputs.precision_embargo50_bucket_arn}/*",
+      data.terraform_remote_state.africa_south_region.outputs.af_south_s3_storage_bucket_arn,
+      "${data.terraform_remote_state.africa_south_region.outputs.af_south_s3_storage_bucket_arn}/*",
       data.terraform_remote_state.africa_south_region.outputs.af_south_s3_discover_bucket_arn,
       "${data.terraform_remote_state.africa_south_region.outputs.af_south_s3_discover_bucket_arn}/*",
+      data.terraform_remote_state.africa_south_region.outputs.af_south_s3_embargo_bucket_arn,
+      "${data.terraform_remote_state.africa_south_region.outputs.af_south_s3_embargo_bucket_arn}/*",
 
     ]
   }
@@ -160,6 +164,9 @@ data "aws_iam_policy_document" "ecs_task_iam_policy_document" {
       "${data.terraform_remote_state.platform_infrastructure.outputs.precision_embargo50_bucket_arn}/*",
       data.terraform_remote_state.africa_south_region.outputs.af_south_s3_discover_bucket_arn,
       "${data.terraform_remote_state.africa_south_region.outputs.af_south_s3_discover_bucket_arn}/*",
+      data.terraform_remote_state.africa_south_region.outputs.af_south_s3_embargo_bucket_arn,
+      "${data.terraform_remote_state.africa_south_region.outputs.af_south_s3_embargo_bucket_arn}/*",
+      
     ]
   }
 
@@ -178,6 +185,7 @@ data "aws_iam_policy_document" "ecs_task_iam_policy_document" {
       data.terraform_remote_state.platform_infrastructure.outputs.precision_publish50_bucket_arn,
       data.terraform_remote_state.platform_infrastructure.outputs.precision_embargo50_bucket_arn,
       data.terraform_remote_state.africa_south_region.outputs.af_south_s3_discover_bucket_arn,
+      data.terraform_remote_state.africa_south_region.outputs.af_south_s3_embargo_bucket_arn,
       
     ]
   }
@@ -196,7 +204,8 @@ data "aws_iam_policy_document" "ecs_task_iam_policy_document" {
       data.terraform_remote_state.platform_infrastructure.outputs.rejoin_embargo50_bucket_arn,
       data.terraform_remote_state.platform_infrastructure.outputs.precision_publish50_bucket_arn,
       data.terraform_remote_state.platform_infrastructure.outputs.precision_embargo50_bucket_arn,
-      data.terraform_remote_state.af_south_1.outputs.af_south_s3_bucket_arn,
+      data.terraform_remote_state.africa_south_region.outputs.af_south_s3_discover_bucket_arn,
+      data.terraform_remote_state.africa_south_region.outputs.af_south_s3_embargo_bucket_arn,
     ]
   }
 
@@ -222,6 +231,10 @@ data "aws_iam_policy_document" "ecs_task_iam_policy_document" {
       "${data.terraform_remote_state.platform_infrastructure.outputs.precision_publish50_bucket_arn}/*",
       data.terraform_remote_state.platform_infrastructure.outputs.precision_embargo50_bucket_arn,
       "${data.terraform_remote_state.platform_infrastructure.outputs.precision_embargo50_bucket_arn}/*",
+      data.terraform_remote_state.africa_south_region.outputs.af_south_s3_discover_bucket_arn,
+      "${data.terraform_remote_state.africa_south_region.outputs.af_south_s3_discover_bucket_arn}/*",
+      data.terraform_remote_state.africa_south_region.outputs.af_south_s3_embargo_bucket_arn,
+      "${data.terraform_remote_state.africa_south_region.outputs.af_south_s3_embargo_bucket_arn}/*",
     ]
   }
 
@@ -247,6 +260,10 @@ data "aws_iam_policy_document" "ecs_task_iam_policy_document" {
       "${data.terraform_remote_state.platform_infrastructure.outputs.precision_publish50_bucket_arn}/*",
       data.terraform_remote_state.platform_infrastructure.outputs.precision_embargo50_bucket_arn,
       "${data.terraform_remote_state.platform_infrastructure.outputs.precision_embargo50_bucket_arn}/*",
+      data.terraform_remote_state.africa_south_region.outputs.af_south_s3_discover_bucket_arn,
+      "${data.terraform_remote_state.africa_south_region.outputs.af_south_s3_discover_bucket_arn}/*",
+      data.terraform_remote_state.africa_south_region.outputs.af_south_s3_embargo_bucket_arn,
+      "${data.terraform_remote_state.africa_south_region.outputs.af_south_s3_embargo_bucket_arn}/*",
     ]
   }
 
