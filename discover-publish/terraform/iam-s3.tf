323bc41a5e1696c38518fcd220a9b67cf67457b2
ddefreitas.exe <1959720+darrell-d@users.noreply.github.com>
2025-11-26 10:49:03 -0500

    add missing comma

--- discover-publish/terraform/iam-s3.tf
+++ discover-publish/terraform/iam-s3.tf
@@ -140,7 +140,7 @@ data "aws_iam_policy_document" "discover_publish_s3_write_access_iam_policy_docu
       data.terraform_remote_state.platform_infrastructure.outputs.precision_publish50_bucket_arn,
       "${data.terraform_remote_state.platform_infrastructure.outputs.precision_publish50_bucket_arn}/*",
       data.terraform_remote_state.platform_infrastructure.outputs.precision_embargo50_bucket_arn,
-      "${data.terraform_remote_state.platform_infrastructure.outputs.precision_embargo50_bucket_arn}/*"
+      "${data.terraform_remote_state.platform_infrastructure.outputs.precision_embargo50_bucket_arn}/*",
 
       data.terraform_remote_state.platform_infrastructure.outputs.awsod_edots_publish50_bucket_arn,
       "${data.terraform_remote_state.platform_infrastructure.outputs.awsod_edots_publish50_bucket_arn}/*",
